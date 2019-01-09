8fdf6989f2bb8cd75813ea437ba7f4bc65ae57db
d3x4r <dex3r@mail.ru>
2019-01-09 20:12:37 +0500

    Шапка мобильной и планшетной версии

--- gulpfile.js
+++ gulpfile.js
@@ -45,6 +45,7 @@ gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
     "source/img/*",
+    "source/pixelglass/**/*",
     '!source/img/sprite'
   ], {
       base: "source"
