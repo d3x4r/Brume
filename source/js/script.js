f6b7757893e7d09a4bbb19a5f73a543ad9ea4e38
d3x4r <dex3r@mail.ru>
2019-01-11 19:54:03 +0500

    Блок Преимущества и поправлен скрип показа меню

--- source/js/script.js
+++ source/js/script.js
@@ -4,27 +4,25 @@ var ESC_KEYCODE = 27;
 var menuButton = document.querySelector('.site-navigation__toggle');
 var mainMenu = document.querySelector('.site-navigation__list');
 
-var onButtonKeydown = function (evt) {
-  if (evt.keyCode === ESC_KEYCODE) {
+
+var closeMenu = function () {
     menuButton.classList.remove('site-navigation__toggle--open');
     mainMenu.classList.remove('site-navigation__list--open');
-    document.removeEventListener('keydown', onButtonKeydown);
-  }
 };
 
-var changeMenuClass = function () {
+var changeMenuStatus = function () {
   menuButton.classList.toggle('site-navigation__toggle--open');
   mainMenu.classList.toggle('site-navigation__list--open');
 };
 
 var onButtonKeydown = function (evt) {
   if (evt.keyCode === ESC_KEYCODE) {
-    changeMenuClass();
+    closeMenu();
     document.removeEventListener('keydown', onButtonKeydown);
   }
 };
 
 menuButton.addEventListener('click', function () {
-  changeMenuClass();
+  changeMenuStatus();
   document.addEventListener('keydown', onButtonKeydown);
 });
